b801576a673252366c3d6c57a2e241de7cca631c
Laboratorio <laboratorio@udla-ec.int>
2026-04-22 11:33:25 -0500

    Eduardo Cisneros main correjido

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        UsuarioSeguroAvanzado userExito=new UsuarioSeguroAvanzado("Eduardo", "Admin123", 3)
+        UsuarioSeguroAvanzado userExito = new UsuarioSeguroAvanzado("Eduardo", "Admin123", 3);
 
-
-
-
-
-       // Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("=== SISTEMA DE USUARIO SEGURO ===");
 
@@ -25,7 +21,7 @@ public class Main {
         int maxIntentos = sc.nextInt();
         sc.nextLine(); // limpiar buffer
 
-        //UsuarioSeguroAvanzado usuario = new UsuarioSeguroAvanzado(username, password, maxIntentos);
+        UsuarioSeguroAvanzado usuario = new UsuarioSeguroAvanzado(username, password, maxIntentos);
 
         int opcion;
 
